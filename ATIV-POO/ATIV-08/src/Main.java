ddb1ac507698e5b392154827ae530a1401ee16ee
Arthur reis <arthur88727771@gmail.com>
2024-03-27 09:53:28 -0300

    Fiz o Menu

--- ATIV-POO/ATIV-08/src/Main.java
+++ ATIV-POO/ATIV-08/src/Main.java
@@ -1,10 +1,39 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         ArrayList<Produto> produtos = new ArrayList<>(); // cria um array para armazenar os produtos
 
+        Menu(produtos); // chamar o método menu
+
+    }
+
+    // USADO PARA FAZER O MENU E TE LEVAR PARA O LUGAR
+    public static void Menu(ArrayList<Produto> produtos){
+        Scanner read = new Scanner(System.in);
+
+        int Menu = 0;
+        System.out.println("---------- - MENU - -----------");
+        System.out.println("--------- LISTAR  - 1 ---------");
+        System.out.println("--------- INSERIR - 2 ---------");
+        System.out.println(" ");
+
+        System.out.println(" INFORME OQUE VOCE QUER FAZER: ");
+        Menu = read.nextInt();
+        if (Menu == 1){
+            ListarProd(produtos);
+        } else if (Menu == 2) {
+            InserirProd(produtos);
+        } else {
+            System.out.println("ATÉ A PROXIMA!!!");
+        }
+    }
+
+    // USADO PARA INSERIR O PRODUTO
+    public static void InserirProd(ArrayList<Produto> produtos){
+        Scanner read = new Scanner(System.in);
         boolean sn;
         System.out.println("Você deseja inserir um produto? (s)im (n)ão");
         sn = read.nextBoolean();
@@ -28,20 +57,13 @@ public class Main {
 
             System.out.println("Você deseja inserir mais um produto? (true ou false)");
             sn = read.nextBoolean();
+
             read.nextLine(); // Limpa o buffer do scanner
         }
-
-        // chama o método listar
-        boolean listar;
-        System.out.println("Você deseja listar os produtos? (true ou false)");
-        listar = read.nextBoolean();
-
-        if (listar){ // passando para o método
-            ListarProd(produtos);
-        }
-
-        read.close(); // Fecha o Scanner
+        Menu(produtos); // volta para o método MENU
     }
+
+    // LISTAR OS PRODUTOS
     public static void ListarProd(ArrayList<Produto> produtos){
 
         System.out.println("\nLista de Produtos:");
@@ -52,5 +74,6 @@ public class Main {
                 System.out.println("Quantidade: " + produto.getQtde());
                 System.out.println();
             }
+        Menu(produtos); // volta para o método MENU
     }
 }
